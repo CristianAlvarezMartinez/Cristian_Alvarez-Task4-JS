a37924d74471c4705094a8201ba69c5585c53d9a
CrisDev <alvarezmartinez132@gmail.com>
2023-04-09 21:10:12 -0500

    Task3 Cambios

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -3,7 +3,7 @@ const cardsContainer = document.getElementById('cards-container')
 const checkboxContainer = document.getElementById('checkbox-container')
 const input = document.getElementById('inputSearch')
 const arrayEventos = data.eventos;
-let arrayElementos
+
 // CHECKBOX DINAMICOS RECURSOS
 
 const categories = arrayEventos.map(evento => evento.category)
@@ -14,47 +14,24 @@ const arrayCategories = Array.from(setCategories)
 printCards(arrayEventos)
 printCategories(arrayCategories)
 
-
 // EVENTOS
 
-  checkboxContainer.addEventListener('change', () =>{
-    let check = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'))
-    check.forEach(element => {
-      while (element.checked == true) {
-        filterByCategory(arrayEventos, element)
-      }
-    })
-  })
-
+checkboxContainer.addEventListener('change',()=>{
+  let aux = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'))
+  printCards(combinedFilter(aux, inputSearch.value))
+})
 
 const inputSearch = document.getElementById('inputSearch')
 inputSearch.addEventListener('keyup',() => {
-  printCards(combinedFilter(arrayEventos, inputSearch, inputSearch.value))
+  printCards(filterByText(arrayEventos, inputSearch.value))
 })
 
 
 
 // FUNCIONES
-function template(array) {
- return array.reduce((acu, acc) => {
-    return acu + `<div class="card">
-                    <img src="${acc.image}" class="card-img-top" alt="imgCard">
-                    <div class="card-body text-center">
-                    <h5 class="card-title">${acc.name}</h5>
-                    <p class="card-text">${acc.description}</p>
-                    <div class="priceBtn mhere gap-4">
-                    <p class="m-0">Price: $${acc.price}</p>
-                    <a href="./assets/pages/details.html?id=${acc.name}" class="btn btn-primary">More Info</a>
-                    </div>
-                    </div>
-                  </div>`
-  },'')
-}
 
 function printCategories(array) {
-  if(array.length == 0){
-    checkboxContainer.innerHTML = `<h1>NO HAY ELEMENTOS PARA MOSTRAR</h1>`
-  }
+
   let template = array.reduce((acu, acc) => {
           return acu + `<div class="form-check checkboxs col-5 col-lg-4 col-xxl-1 d-flex justify-content-center gap-2 p-0">
                     <input class="form-check-input" type="checkbox" value="${acc.replace(/\s+/g, '')}" id="${acc.replace(/\s+/g, '')}">
@@ -65,9 +42,7 @@ function printCategories(array) {
 }
 
 function printCards(array) {
-  if(array.length == 0){
-    cardsContainer.innerHTML = `<h1 class="text-center text-light">THERE'S NO FOUND</h1>`
-  }else {
+
     let template = array.reduce((acu, acc) => {
       return acu + `<div class="card">
                       <img src="${acc.image}" class="card-img-top" alt="imgCard">
@@ -82,24 +57,30 @@ function printCards(array) {
                     </div>`
     },'')
     cardsContainer.innerHTML = template
-  }
-
-  
 }
 
 function filterByText(array, txt) {
-  return array.filter(evento => evento.name.toLowerCase().includes(txt.toLowerCase()) || evento.description.toLowerCase().includes(txt.toLowerCase()))
+  return array.filter(evento => evento.name.toLowerCase().includes(txt.toLowerCase()))
 }
 
-function filterByCategory(array, element) {
-  if (element.check == true) {
-    return array.filter(evento => evento.category.replace(/\s+/g, '') === element.id)  
+function filterByCategory(array) {
+  let arrayFilter = [];
+  array.reduce((acu, acc)=>{
+    arrayEventos.filter(evento => {
+      if(evento.category.replace(/\s+/g, '') === acc.id){
+        arrayFilter.push(evento)
+      }
+    })
+  },'')
+  if(arrayFilter.length == 0){
+    return arrayEventos
   }
-  return array
+  return arrayFilter
 }
 
-function combinedFilter(array, element, txt) {
-  const filteredByCategory = filterByCategory(array, element)
-  const filteredByText = filterByText(filteredByCategory, txt)
-  return filteredByText
+function combinedFilter(array, txt) {
+    const filteredByCategory = filterByCategory(array)
+    const filteredByText = filterByText(filteredByCategory, txt)
+    return filteredByText
+  
 }
--- assets/JS/upComingEvents.js
+++ assets/JS/upComingEvents.js
@@ -1,44 +1,104 @@
 // CARDS DINAMICAS
 const cardsContainer = document.getElementById('cards-container')
-let arrayEvento = data.eventos
+let arrayEventos = data.eventos
 let fecha = data.fechaActual
 fecha = parseInt(fecha.replace(/-/g, '')) //20220101
-
-const filtroEventos = (array, fecha) => {
-    let ux =array.filter(i => parseInt(i.date.replace(/-/g, '')) > fecha)
-    let template = ux.reduce((acu, acc) => {
-        return acu + `<div class="card">
-                        <img src="${acc.image}">
-                        <div class="card-body text-center">
-                        <h5 class="card-title">${acc.name}</h5>
-                        <p class="card-text">${acc.description}</p>
-                        <div class="priceBtn gap-4">
-                            <p class="m-0">Price: $${acc.price}</p>
-                            <a href="../pages/details.html?id=${acc.name}" class="btn btn-primary">More Info</a>
-                        </div>
-                        </div>
-                      </div>
-`
-    },'')
-    cardsContainer.innerHTML = template
-}
-filtroEventos(arrayEvento, fecha)
-
+let eventosFiltrados = arrayEventos.filter(i => parseInt(i.date.replace(/-/g, '')) > fecha)
 
 // CHECKBOX DINAMICOS
-
-const categories = arrayEvento.map(evento => evento.category)
+const categories = arrayEventos.map(evento => evento.category)
 const setCategories = new Set(categories)
 const arrayCategories = Array.from(setCategories)
-
 const checkboxContainer = document.getElementById('checkbox-container')
-const printCategories = array => {
+
+// EJECUCIONES
+printCategories(arrayCategories)
+filtroEventos(arrayEventos, fecha)
+
+// EVENTOS
+
+checkboxContainer.addEventListener('change',()=>{
+  let aux = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'))
+  printCards(combinedFilter(aux, inputSearch.value))
+})
+
+const inputSearch = document.getElementById('inputSearch')
+inputSearch.addEventListener('keyup',() => {
+  printCards(filterByText(eventosFiltrados, inputSearch.value))
+})
+
+// FUNCIONES
+
+function filtroEventos(array, fecha){
+  let ux = array.filter(i => parseInt(i.date.replace(/-/g, '')) > fecha)
+  let template = ux.reduce((acu, acc) => {
+      return acu + `<div class="card">
+                      <img src="${acc.image}">
+                      <div class="card-body text-center">
+                      <h5 class="card-title">${acc.name}</h5>
+                      <p class="card-text">${acc.description}</p>
+                      <div class="priceBtn gap-4">
+                          <p class="m-0">Price: $${acc.price}</p>
+                          <a href="../pages/details.html?id=${acc.name}" class="btn btn-primary">More Info</a>
+                      </div>
+                      </div>
+                    </div>
+`
+  },'')
+  cardsContainer.innerHTML = template
+}
+
+function printCategories(array) {
   let template = array.reduce((acu, acc) => {
           return acu + `<div class="form-check checkboxs col-5 col-lg-4 col-xxl-1 d-flex justify-content-center gap-2 p-0">
-                    <input class="form-check-input" type="checkbox" value="${acc}" id="flexCheckIndeterminate2">
-                    <label class="form-check-label" for="flexCheckIndeterminate2">${acc}</label>
+                    <input class="form-check-input" type="checkbox" value="${acc.replace(/\s+/g, '')}" id="${acc.replace(/\s+/g, '')}">
+                    <label class="form-check-label" for="${acc.replace(/\s+/g, '')}">${acc}</label>
                   </div>`
   } ,'')
   checkboxContainer.innerHTML += template
 }
-printCategories(arrayCategories)
+
+function printCards(array) {
+
+    let template = array.reduce((acu, acc) => {
+      return acu + `<div class="card">
+                      <img src="${acc.image}" class="card-img-top" alt="imgCard">
+                      <div class="card-body text-center">
+                      <h5 class="card-title">${acc.name}</h5>
+                      <p class="card-text">${acc.description}</p>
+                      <div class="priceBtn mhere gap-4">
+                      <p class="m-0">Price: $${acc.price}</p>
+                      <a href="./assets/pages/details.html?id=${acc.name}" class="btn btn-primary">More Info</a>
+                      </div>
+                      </div>
+                    </div>`
+    },'')
+    cardsContainer.innerHTML = template
+}
+
+function filterByText(array, txt) {
+  return array.filter(evento => evento.name.toLowerCase().includes(txt.toLowerCase()))
+}
+
+function filterByCategory(array) {
+  let arrayFilter = [];
+  array.reduce((acu, acc)=>{
+    eventosFiltrados.filter(evento => {
+      if(evento.category.replace(/\s+/g, '') === acc.id){
+        arrayFilter.push(evento)
+      }
+    })
+  },'')
+  if(arrayFilter.length == 0){
+    return eventosFiltrados
+  }
+  return arrayFilter
+}
+
+
+function combinedFilter(array, txt) {
+  const filteredByCategory = filterByCategory(array)
+  const filteredByText = filterByText(filteredByCategory, txt)
+  return filteredByText
+
+}
